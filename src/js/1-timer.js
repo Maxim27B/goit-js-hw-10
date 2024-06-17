ace8387b9cd83ecfa3a539e6f48c0e8365dbae59
Maxim_Boiko <Maksimboiko222@gmail.com>
2024-06-17 12:26:45 +0300

    js hw 10 task 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,87 +1,86 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const images = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
-
-const list = document.querySelector('ul');
-let markup = '';
-for (const { preview, original, description } of images) {
-  const template = `<li class="gallery-item">
-        <a class="gallery-link" href="${original}">
-          <img
-            class="gallery-image"
-            src="${preview}"
-            alt="${description}"
-          />
-        </a>
-      </li>`;
-  markup += template;
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('[data-start]'),
+  daysElem: document.querySelector('[data-days]'),
+  hoursElem: document.querySelector('[data-hours]'),
+  minutesElem: document.querySelector('[data-minutes]'),
+  secondsElem: document.querySelector('[data-seconds]'),
 }
-list.innerHTML = [markup];
 
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+refs.btn.disabled = true;
+let userSelectedDate;
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+   onClose(selectedDates) {
+     if (selectedDates[0] > Date.now()) {
+       userSelectedDate = selectedDates[0];
+       refs.btn.disabled = false;
+     } else {
+       iziToast.error({
+         title: 'Error',
+         message: 'Please choose a date in the future',
+         position: 'topCenter',
+         backgroundColor: 'red',
+         theme: 'dark',
+         messageColor: 'white',
+       });
+       refs.btn.disabled = true;
+     };
+  },
+})
+
+let intervalId;
+refs.btn.addEventListener('click', () => {
+  const initDate = userSelectedDate;
+  let diff;
+  setTimeout(() => {
+    clearInterval(intervalId);
+    refs.input.disabled = false;
+  }, initDate - Date.now())
+
+  intervalId = setInterval(() => {
+    const currentDate = Date.now();
+    diff = initDate - currentDate;
+    const time = convertMs(diff);
+    getTime(time);
+  }, 1000);
+
+  refs.btn.disabled = true;
+  refs.input.disabled = true;
+})
+
+function getTime({ days, hours, minutes, seconds }) {
+  refs.daysElem.textContent = addLeadingZero(days);
+  refs.hoursElem.textContent = addLeadingZero(hours);
+  refs.minutesElem.textContent = addLeadingZero(minutes);
+  refs.secondsElem.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  value = value.toString().padStart(2, 0);
+  return value;
+}
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
